650d9581202b02cb04b5ac72a49d6b3d1d848617
Danil Falaleev <danilkafalaleev@gmail.com>
2024-04-19 01:46:46 +0300

    add render api

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const API = axios.create({
     // We are using apisful.com as a backend for this e-commerce website
-    baseURL: 'https://66216fb7f9269513c921d57c--prismatic-cascaron-8c0697.netlify.app/',
+    baseURL: 'https://mdk-api.onrender.com/',
     // headers: {
     //     // Put your access key here
     //     'X-Api-Key': '-u3Vg7mF0cf5_lRS0Ir8GpFtQMT9mpIQc-VXHN6yCjI'
